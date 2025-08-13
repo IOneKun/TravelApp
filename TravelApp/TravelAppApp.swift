8c6a172a40d406827cf25dff621ab72fac599bf7
IvanKuninets <ivankuninec8@gmail.com>
2025-08-13 17:50:55 +0200

    Initial Commit

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Ivan Kuninets on 8/13/25.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
